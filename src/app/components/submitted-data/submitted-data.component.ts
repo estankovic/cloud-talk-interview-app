b701c7d3ae35a0611a695f65661c50ede28a8b3a
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2021-03-31 14:29:17 +0200

    SubmittedDataComponent now accepts inputs

--- src/app/components/submitted-data/submitted-data.component.ts
+++ src/app/components/submitted-data/submitted-data.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-submitted-data',
   templateUrl: './submitted-data.component.html',
-  styleUrls: ['./submitted-data.component.scss']
+  styleUrls: ['./submitted-data.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SubmittedDataComponent implements OnInit {
+
+  @Input() data: {name: string; email: string; phone_number: string} = undefined;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
