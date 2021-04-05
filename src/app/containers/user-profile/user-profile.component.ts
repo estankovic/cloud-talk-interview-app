2f8a527a690f1dd572557fdf9fe15fab4ce43c8d
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2021-04-05 12:04:13 +0200

    Added Styling Logic to UserProfile Container

--- src/app/containers/user-profile/user-profile.component.ts
+++ src/app/containers/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, Renderer2} from '@angular/core';
 import {AppStateFacade} from "../../services/app-state.facade";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-user-profile',
@@ -9,18 +10,32 @@ import {AppStateFacade} from "../../services/app-state.facade";
 })
 export class UserProfileComponent implements OnInit {
 
+  @Input() customBgColor: string;
+
   profileFormData$ = this.appState.profileFormData$;
-  profileFormSubmittedSuccessFully$ = this.appState.profileFormSubmittedSuccessfully$;
+  profileFormSubmittedSuccessFully$ = this.appState.profileFormSubmittedSuccessfully$.pipe(
+    tap(submitted => this.changeBgColor(submitted))
+  );
   isLoading$ = this.appState.userDataLoading$;
   isLoaded$ = this.appState.userDataLoaded$;
   hasError$ = this.appState.userDataHasError$;
 
   constructor(
     private readonly appState: AppStateFacade,
+    private readonly renderer: Renderer2,
+    private readonly el: ElementRef,
   ) { }
 
   ngOnInit() {
     this.appState.loadUserData();
   }
 
+  changeBgColor(formStatus: boolean) {
+    console.log(formStatus)
+    if (formStatus && this.customBgColor) {
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', this.customBgColor)
+    } else {
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'white')
+    }
+  }
 }
